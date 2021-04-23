65b464e228f7a69a3b3e6b8064b83e8270b40e8f
Tendai Nyandoro <tendai1@hotmail.com>
2021-04-23 02:05:49 +0200

    Fix nm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+import _ from 'lodash';
+import { $, jQuery } from 'jquery';
+import { compareAsc, format } from 'date-fns';
+
+format(new Date(2014, 1, 11), 'yyyy-MM-dd');
+//= > '2014-02-11'
+
+const dates = [
+  new Date(1995, 6, 2),
+  new Date(1987, 1, 11),
+  new Date(1989, 6, 10),
+];
+dates.sort(compareAsc);
+//= > [
+//   Wed Feb 11 1987 00:00:00,
+//   Mon Jul 10 1989 00:00:00,
+//   Sun Jul 02 1995 00:00:00
+// ]
+// export for others scripts to use
+window.$ = $;
+window.jQuery = jQuery;
+
 function component() {
   const element = document.createElement('div');
 
