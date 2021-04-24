4abfe5e50210c574c23d29279e80357f5a3ea139
Tendai Nyandoro <tendai1@hotmail.com>
2021-04-24 16:10:01 +0200

    Declare the date API

--- src/index.js
+++ src/index.js
@@ -1,39 +1,10 @@
-import _ from 'lodash';
-import { $, jQuery } from 'jquery';
-import { compareAsc, format } from 'date-fns';
-import Icon from './icon.png';
-import printMe from './print.js';
+import { format, formatDistance, formatRelative, subDays } from 'date-fns'
 
-// format(new Date(2014, 1, 11), 'yyyy-MM-dd');
-// => '2014-02-11'
+format(new Date(), "'Today is a' eeee")
+//=> "Today is a Saturday"
 
-// const dates = [
-//   new Date(1995, 6, 2),
-//   new Date(1987, 1, 11),
-//   new Date(1989, 6, 10),
-// ];
-// dates.sort(compareAsc);
-// => [
-//   Wed Feb 11 1987 00:00:00,
-//   Mon Jul 10 1989 00:00:00,
-//   Sun Jul 02 1995 00:00:00
-// ]
-// export for others scripts to use
-window.$ = $;
-window.jQuery = jQuery;
+formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true })
+//=> "3 days ago"
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-btn.innerHTML = 'Click me and check the console!';
-btn.onclick = printMe;
-
-element.appendChild(btn);
-
-  return element;
-}
-
-document.body.appendChild(component());
+formatRelative(subDays(new Date(), 3), new Date())
+//=> "last Friday at 7:26 p.m."
