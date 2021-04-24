c57b281046f5b38ed0cb8b2e7229fa9e0a00f05d
Tendai Nyandoro <tendai1@hotmail.com>
2021-04-24 03:13:51 +0200

    Scss

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import _ from 'lodash';
 import { $, jQuery } from 'jquery';
 import { compareAsc, format } from 'date-fns';
+import Icon from './icon.png';
+import printMe from './print.js';
 
-format(new Date(2014, 1, 11), 'yyyy-MM-dd');
-//= > '2014-02-11'
+// format(new Date(2014, 1, 11), 'yyyy-MM-dd');
+// => '2014-02-11'
 
-const dates = [
-  new Date(1995, 6, 2),
-  new Date(1987, 1, 11),
-  new Date(1989, 6, 10),
-];
-dates.sort(compareAsc);
-//= > [
+// const dates = [
+//   new Date(1995, 6, 2),
+//   new Date(1987, 1, 11),
+//   new Date(1989, 6, 10),
+// ];
+// dates.sort(compareAsc);
+// => [
 //   Wed Feb 11 1987 00:00:00,
 //   Mon Jul 10 1989 00:00:00,
 //   Sun Jul 02 1995 00:00:00
@@ -22,9 +24,14 @@ window.jQuery = jQuery;
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+btn.innerHTML = 'Click me and check the console!';
+btn.onclick = printMe;
+
+element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
   output: {
-    filename: 'main.js',
+    // filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      ],
   },
 };
