cd96141111556638fd63c22e18def098f14916a0
Tendai Nyandoro <tendai1@hotmail.com>
2021-05-01 23:13:54 +0200

    Setup the web and configure

--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,23 @@ const path = require('path');
 module.exports = {
   entry: {
     index: './src/index.js',
-    print: './src/print.js',
   },
   output: {
     // filename: 'bundle.js',
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
-      test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
-        }
-      }
-    }
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
     ],
   },
