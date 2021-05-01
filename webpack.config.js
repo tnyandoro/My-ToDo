779d1eaed0fa539b6551d2b4a863dea8e38020e9
Tendai Nyandoro <tendai1@hotmail.com>
2021-05-01 22:31:35 +0200

    Add the babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,16 @@ module.exports = {
   module: {
     rules: [
       {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
