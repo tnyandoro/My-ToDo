9de04e113f8c6e7e16e7738d192776ebbee91de7
Tendai Nyandoro <tendai1@hotmail.com>
2021-05-02 14:55:00 +0200

    Let declare the static project method in the Project class

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,8 +1,13 @@
 import MyTodo from './mytodo';
+var json = require('json');
 
 class Project {
   constructor(name) {
     this.name = name;
     this.todos = [];
+  }
+
+  static fromJSON(json) {
+    const project = new Project(json.name);
   }
 }
