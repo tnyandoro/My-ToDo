d74e15e6be7f3982ca2024e884d50cfb27b43f00
Tendai Nyandoro <tendai1@hotmail.com>
2021-05-02 18:41:33 +0200

    Add the method to add projects to the site page

--- src/modules/site.js
+++ src/modules/site.js
@@ -4,6 +4,14 @@ class Site {
   constructor() {
     const description = 'Describe your Todo';
     const priority = 'high';
-    const dueDate = new Date(),
+    const dueDate = '2021, 4, 02',
+
+    const p1 = new Project('Project 1'); 
+    p1.addMyTodo('MyTodo 1', description, dueDate, priority);
+    p1.addMyTodo('MyTodo 2', description, '2021, 4, 02', 'low');
+    p1.addMyTodo('MyTodo 3', description, dueDate, 'medium');
+
+    this.projects = [p1];
+    this.activeProjectIndex = 0;
   }
 }
