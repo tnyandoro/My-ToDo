da711226a57b6959087c145ef64add9b949bd268
Tendai Nyandoro <tendai1@hotmail.com>
2021-05-03 13:58:18 +0200

    Set the main js file

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import _ from 'lodash';
+import $ from 'jquery'
 import './assets/style.css';
 import Icon from './assets/icon.png';
+import Site from './modules/site';
+import MyTodo from './modules/mytodo';
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  return element;
-}
-
-document.body.appendChild(component());
