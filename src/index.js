1b738b6e3e9957d7aee90a35e1ed24fd3ec2d34e
Tendai Nyandoro <tendai1@hotmail.com>
2021-05-03 14:19:01 +0200

    Create a method to render on the page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import _ from 'lodash';
-import $ from 'jquery'
+import $ from 'jquery';
 import './assets/style.css';
 import Icon from './assets/icon.png';
 import Site from './modules/site';
 import MyTodo from './modules/mytodo';
 
+const renderProjects = (projects, activeProjectIndex) => {
+  const projectsContainer = $('#projects');
+  projectsContainer.empty();
+  projects.forEach((project, index) => {
+    projectsContainer.append(
+      `<li class "project project${index}" data-id=${index}>${project.getName()}</li>`,
+    );
+  });
+  $('.project').removeClass('active');
+  $(`.project[data-id=${activeProjectIndex}]`).addClass('active');
+};
